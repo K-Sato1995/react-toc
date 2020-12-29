5f7078c3eb1844f7fda09598ce7a542ceb6b76e1
Katsuki Sato <katsuki_sato0130@icloud.com>
2020-12-29 15:15:36 +0900

    Add eslint

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,29 +1,34 @@
-import { createLink, createTitle, extractHeadingsFromMd, removeCodeBlockFromMd } from "../utils";
+import {
+  createLink,
+  createTitle,
+  extractHeadingsFromMd,
+  removeCodeBlockFromMd,
+} from '../utils'
 
-describe("createLink", () => {
+describe('createLink', () => {
   it("removes # and connects each word with '-'.", () => {
-    expect(createLink("# Test Heading  ")).toEqual("test-heading");
-    expect(createLink("## This is a test heading")).toEqual(
-      "this-is-a-test-heading"
-    );
-  });
+    expect(createLink('# Test Heading  ')).toEqual('test-heading')
+    expect(createLink('## This is a test heading')).toEqual(
+      'this-is-a-test-heading',
+    )
+  })
 
-  it("it replaces /!/? with -", () => {
-    expect(createLink("### Test?")).toEqual("test-");
-  });
-});
+  it('replaces /!/? with -', () => {
+    expect(createLink('### Test?')).toEqual('test-')
+  })
+})
 
-describe("createTitle", () => {
-  it("removes #.", () => {
-    expect(createTitle("# Test Heading", 100)).toEqual("Test Heading");
-  });
+describe('createTitle', () => {
+  it('removes #.', () => {
+    expect(createTitle('# Test Heading', 100)).toEqual('Test Heading')
+  })
 
-  it("shortens the string", () => {
-    expect(createTitle("# Test Heading", 6)).toEqual("Test H..");
-  });
-});
+  it('shortens the string', () => {
+    expect(createTitle('# Test Heading', 6)).toEqual('Test H..')
+  })
+})
 
-describe("extractHeadingsFromMd", () => {
+describe('extractHeadingsFromMd', () => {
   const markdownText = `
 # Heading1
   This is the first paragraph.
@@ -31,38 +36,36 @@ describe("extractHeadingsFromMd", () => {
   This is the second paragraph.
 ### Heading3
   This is the third paragraph.
-  `;
+  `
 
-  it("It extracts headings from the given markdownText.", () => {
+  it('extracts 1-3 headings from the given markdownText.', () => {
     expect(extractHeadingsFromMd(markdownText, 1, 3)).toEqual([
-      "# Heading1\n",
-      "## Heading2\n",
-      "### Heading3\n"
-    ]);
-  });
+      '# Heading1\n',
+      '## Heading2\n',
+      '### Heading3\n',
+    ])
+  })
 
-  it("It extracts headings from the given markdownText.", () => {
+  it('extracts 1-2 headings from the given markdownText.', () => {
     expect(extractHeadingsFromMd(markdownText, 1, 2)).toEqual([
-      "# Heading1\n",
-      "## Heading2\n"
-    ]);
-  });
+      '# Heading1\n',
+      '## Heading2\n',
+    ])
+  })
 
-  it("It extracts headings from the given markdownText.", () => {
+  it('extracts 2-3 headings from the given markdownText.', () => {
     expect(extractHeadingsFromMd(markdownText, 2, 3)).toEqual([
-      "## Heading2\n",
-      "### Heading3\n"
-    ]);
-  });
+      '## Heading2\n',
+      '### Heading3\n',
+    ])
+  })
 
-  it("It extracts headings from the given markdownText.", () => {
-    expect(extractHeadingsFromMd(markdownText, 2, 2)).toEqual([
-      "## Heading2\n"
-    ]);
-  });
-});
+  it('extracts headings from the given markdownText.', () => {
+    expect(extractHeadingsFromMd(markdownText, 2, 2)).toEqual(['## Heading2\n'])
+  })
+})
 
-describe("removeCodeBlockFromMd", () => {
+describe('removeCodeBlockFromMd', () => {
   const markdownText = `
 # Heading1
   This is the first paragraph.
@@ -83,9 +86,9 @@ Text between codeblock
 ### Escaped codeblock
 \`\`\`
 ~~~
-  `;
+  `
 
-  it("It removes codeblock from the given markdownText.", () => {
+  it('removes codeblock from the given markdownText.', () => {
     expect(removeCodeBlockFromMd(markdownText)).toEqual(`
 # Heading1
   This is the first paragraph.
@@ -93,7 +96,6 @@ Text between codeblock
   This is the second paragraph.
 Text between codeblock
 Text between codeblock
-  `
-    );
-  });
-});
+  `)
+  })
+})
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -16,3 +16,40 @@ declare module "*.svg" {
   export default svgUrl;
   export { svgComponent as ReactComponent };
 }
+
+interface TocProps {
+  /*
+    The markdown text you want to creat a TOC from.
+   */
+  markdownText: string
+  /*
+    The maximum length of each title in the TOC.
+  */
+  titleLimit?: number | undefined
+  /*
+    The highest level of headings you want to extract from the given markdownText.
+  */
+  highestHeadingLevel?: number
+  /*
+    The lowest level of headings you want to extract from the given markdownText.
+  */
+  lowestHeadingLevel?: number
+  /*
+    The custom className.
+    You can style the TOC like this.
+
+    ```css
+    .customClassName  {
+      border: solid 1px;
+    }
+    .customClassName > li {
+      padding-bottom: 10px;
+    }
+    ```
+  */
+  className?: string
+  /*
+    The type of a TOC you want to use.
+  */
+  type?: 'default' | 'raw' // "fixed-left" | "fixed-right" | "material" | "bootstrap"
+}
